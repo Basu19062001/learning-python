a0cab8c79dd2a055d6617e3bf8271481d326d903
Basudev <samantabasu.2001@gmail.com>
2024-12-20 18:13:57 +0530

    Python started

---
+++ L1 Class/print.py
@@ -0,0 +1,15 @@
+import pandas as pd  # noqa: F401
+
+#This is print function
+print("basu")
+print(4)
+
+'''
+this is triple single code comment method
+Hi this is the first code of my python project
+and this is my first time here
+'''
+
+"""
+hi this is another comment method with triple double code
+"""
