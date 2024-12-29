722c85467ae16168a8836a26510885a17c2056b8
Basudev <samantabasu.2001@gmail.com>
2024-12-29 18:18:38 +0530

    Set operations

--- L6 Class/operations_set.py
+++ L6 Class/operations_set.py
@@ -55,3 +55,61 @@ print(cities1)
 cities1 = {"Tokyo", "Madrid", "Berlin", "Delhi"}
 cities2 = {"Tokyo", "Kolkata", "Berlin", "Hyderabad"}
 print(cities1.isdisjoint(cities2))
+
+cities1 = {""}
+cities2 = {"Tokyo", "Kolkata", "Berlin", "Hyderabad"}
+print(cities1.isdisjoint(cities2))
+
+
+
+cities1 = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+cities2 = {"Tokyo", "Kolkata", "Berlin", "Hyderabad"}
+print(cities1.issuperset(cities2))
+cities3 = {"Madrid", "Delhi"}
+print(cities1.issuperset(cities3))
+
+
+
+cities1 = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+cities2 = {"Kolkata", "Tokyo", "Delhi"}
+print(cities2.issubset(cities1))
+cities3 = {"Berlin", "Delhi"}
+print(cities3.issubset(cities1))
+
+
+cities1= {"Tokyo", "Madrid", "Berlin", "Delhi"}
+cities1.add("Kolkata")
+print(cities1)
+
+
+
+cities1 = {"Tokyo","Madrid","Berlin", "Delhi"}
+cities2 = {"Madrid", "Delhi", "Kolkata"}
+cities1.update(cities2)
+print(cities1)
+
+
+cities1 = {"Tokyo", "Mardrid", "Berlin", "Delhi"}
+cities2 = {"Delhi", "Kolkata", "Hyderabad"}
+cities1.update(cities2)
+print(cities1)
+cities1.remove("Tokyo")
+print(cities1)
+cities1.discard("Delhi")
+print(cities1)
+
+
+cities1 = {"Tokyo", "Mardrid", "Berlin", "Delhi"}
+item = cities1.pop()
+print(item)
+print(cities1)
+
+
+cities1 = {"Tokyo", "Mardrid", "Berlin", "Delhi"}
+cities2 = {"Delhi", "Kolkata", "Hyderabad"}
+cities1.clear()
+print(cities1)
+print(cities2)
+
+del cities2 # set is deleted
+# print(cities2)
--- Practices/practice.py
+++ Practices/practice.py
@@ -1,15 +1,5 @@
-def searchInsert(nums, target):
-  n = len(nums)
-  low = 0
-  high = n- 1
-  ans = n
-  while(low<=high):
-    mid = (low+high)//2
-    if nums[mid] >= target:
-      ans = mid
-      high = mid - 1
-    else:
-      low = mid + 1
-  return ans
-              
-print(searchInsert([1,3,5,6], 4))
+def addTwoNumber(a,b):
+  return a+b
+
+
+print(addTwoNumber(30,20))
